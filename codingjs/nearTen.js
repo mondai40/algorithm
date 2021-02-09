b0797597c5c527432a949fb234736debe213e6bb
40 daimon <tvwmdaimon40@gmail.com>
2021-02-09 05:46:14 -0800

    add nearTen and teenSum solutions

---
+++ codingjs/nearTen.js
@@ -0,0 +1,3 @@
+function nearTen(num: number): boolean {
+  return num % 10 <= 2 || num % 10 >= 8;
+}
---
+++ codingjs/teenSum.js
@@ -0,0 +1,5 @@
+function teenSum(a, b){
+  if a >= 13 && a <= 19 return 19;
+  if b >= 13 && b <= 19 return 19;
+  return a + b;
+}
