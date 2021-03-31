b4bf1e96386c24f805faf851e0adc3b236f5d880
40 daimon <tvwmdaimon40@gmail.com>
2021-03-31 09:21:33 -0700

    create a screen variable

--- planet/game/my_first_game.py
+++ planet/game/my_first_game.py
@@ -1,7 +1,9 @@
 #step1. import pygame
-import Pygame
+import pygame
 
 #step2. define screen size
 screen_size = [360, 600]
 
 #step3. create screen
+#you need to do something on a screen, so create a valuable for screen
+screen = pygame.display.set_mode(screen_size)
