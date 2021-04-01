b87ace4471944eada0290d4a756f32e011d8c293
40 daimon <tvwmdaimon40@gmail.com>
2021-04-01 11:09:50 -0700

    add an image and loat it on the game with pygame.image.load()

--- planet/game/my_first_game.py
+++ planet/game/my_first_game.py
@@ -7,3 +7,8 @@ screen_size = [360, 600]
 #step3. create screen
 #you need to do something on a screen, so create a valuable for screen
 screen = pygame.display.set_mode(screen_size)
+
+#step4. load a image
+#you need to move this image, so create a variable
+#If you have many images to load, you had better criate a function or class
+background = pygame.image.load("./background.jpg");
