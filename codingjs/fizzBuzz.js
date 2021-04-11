15bb7ba87ded193175ff1b378ea639ecd0d46f21
40 daimon <tvwmdaimon40@gmail.com>
2021-04-11 07:36:22 -0700

    add fizzBuzz solutions

---
+++ codingjs/fizzBuzz.js
@@ -0,0 +1,10 @@
+function fizzBuzz(start, end){
+  const result = [];
+  for (let i = start; i < end; i++) {
+    if (i % 3 ===0 && i % 5 === 0) result.push("FizzBuzz");
+    else if (i % 3 === 0) result.push("Fizz"); 
+    else if (i % 5 === 0) result.push("Buzz");
+    else result.push(i.toString());
+  }
+  return result;
+}
