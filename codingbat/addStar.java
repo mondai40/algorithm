0eb11ec8b25d523710cce43d904b02ab4233cfa1
40 daimon <tvwmdaimon40@gmail.com>
2021-05-28 08:57:53 +0900

    add lamda and stream solutions

--- codingbat/addStar.java
+++ codingbat/addStar.java
@@ -1,8 +1,20 @@
 public List<String> addStar(List<String> strings) {
+  // List<String> result = new ArrayList<String>(strings.size());
+  // for (String word: strings) {
+  //   result.add(word + "*");
+  // }
+  // return result;
+  
+  //with lamda
+  // strings.replaceAll(n -> n + "*");
+  // return strings;
+  
+  //with lamda and stream
   List<String> result = new ArrayList<String>(strings.size());
-  for (String word: strings) {
-    result.add(word + "*");
-  }
+  result = strings.stream()
+    .map(n -> n + "*")
+    .collect(Collectors.toList());
+  
   return result;
 }
 
--- codingbat/copies3.java
+++ codingbat/copies3.java
@@ -1,17 +1,28 @@
 public List<String> copies3(List<String> strings) {
+  // List<String> result = new ArrayList<String>(strings.size());
+  // int copyTimes = 3;
+  // for (String word: strings) {
+  //   String newWord = makeCopy(word, copyTimes);
+  //   result.add(newWord);
+  // }
+  // return result;
+  
+  // with lamda
+  // strings.replaceAll(n -> n + n + n);
+  // return strings;
+  
+  // with lamda and stream
   List<String> result = new ArrayList<String>(strings.size());
-  int copyTimes = 3;
-  for (String word: strings) {
-    String newWord = makeCopy(word, copyTimes);
-    result.add(newWord);
-  }
+  result = strings.stream()
+    .map(n -> n + n + n)
+    .collect(Collectors.toList());
   return result;
 }
 
-public String makeCopy(String word, int copyTimes) {
-  String newWord = "";
-  for (int i = 0; i < copyTimes; i++) {
-    newWord += word;
-  }
-  return newWord;
-}
+// public String makeCopy(String word, int copyTimes) {
+//   String newWord = "";
+//   for (int i = 0; i < copyTimes; i++) {
+//     newWord += word;
+//   }
+//   return newWord;
+// }
