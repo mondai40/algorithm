ecd7b12121f224df48da24f951986c89fd89e26a
40 daimon <tvwmdaimon40@gmail.com>
2021-06-17 00:09:37 +0900

    add countTriple solution

---
+++ codingbat/java/countTriple.java
@@ -0,0 +1,12 @@
+public int countTriple(String str) {
+  if (str.length() <= 2) return 0;
+  
+  int count = 0;
+  for (int i = 0; i < str.length() - 2; i++) {
+    if (str.charAt(i) == str.charAt(i + 1) && str.charAt(i) == str.charAt(i + 2)) {
+      count++;
+    }
+  }
+  return count;
+}
+
