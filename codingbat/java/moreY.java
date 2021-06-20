0f3e7dcfa9695b52b46cbda880422b8d241d9f0b
40 daimon <tvwmdaimon40@gmail.com>
2021-06-21 07:33:04 +0900

    add lamda solution on moreY

--- codingbat/java/moreY.java
+++ codingbat/java/moreY.java
@@ -1,5 +1,11 @@
 public List<String> moreY(List<String> strings) {
-  strings.replaceAll(string -> "y" + string + "y");
-  return strings;
+  // strings.replaceAll(string -> "y" + string + "y");
+  // return strings;
+  
+  List<String> result = new ArrayList<>();
+  result = strings.stream()
+    .map(string -> "y" + string + "y")
+    .collect(Collectors.toList());
+  return result;
 }
 
