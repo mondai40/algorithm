5edf265a1774c610988cb7420a4f36b3c7c0efff
40 daimon <tvwmdaimon40@gmail.com>
2021-06-26 12:24:43 +0900

    add count_code solution

---
+++ codingbat/python/count_code.py
@@ -0,0 +1,12 @@
+def count_code(str):
+  count = 0
+  i = 0
+  while i < len(str) - 3:
+    if (str[i:i + 2] == "co" and str[i + 3:i + 4] == "e"):
+      count += 1
+      i += 4
+    else:
+      i += 1
+  return count
+    
+  
