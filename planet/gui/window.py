eaa0e28cf05bd20fce95d09caf88f0c3df175700
40 daimon <tvwmdaimon40@gmail.com>
2021-07-04 00:57:28 +0900

    add first tkinter

---
+++ planet/gui/window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# おまじない1
+root = Tk()
+# windown のタイトル
+root.title("My first window")
+# windown のサイズ, size()ではなく、geometry()に注意。引数の書き方にも注意
+root.geometry("500x500")
+# おまじない2
+root.mainloop()
